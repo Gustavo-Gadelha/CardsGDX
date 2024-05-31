72346b62b0b00969935b5fbae2e6a951b94516ce
Gustavo-Gadelha <gustavofg2112@gmail.com>
2024-05-31 19:28:16 -0300

    Implementado checagem e criação automatica do diretorio do banco de dados

--- core/src/com/cardsgdx/game/database/ConnectionHandler.java
+++ core/src/com/cardsgdx/game/database/ConnectionHandler.java
@@ -1,10 +1,21 @@
 package com.cardsgdx.game.database;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.*;
 
 public class ConnectionHandler {
     private static final String JDBC_SQLITE_DRIVER = "org.sqlite.JDBC";
-    private static final String PLAYERS_DATABASE = "jdbc:sqlite:database//players.db";
+    private static final String PLAYERS_DATABASE = "jdbc:sqlite:database/players.db";
+
+    static {
+        try {
+            Files.createDirectories(Paths.get("database"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     private ConnectionHandler() {}
 
